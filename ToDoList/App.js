241f2f505b2c22781deb44b8855f4d6892570cbc
AhmadHakim2004 <hakimahmad2004@gmail.com>
2024-01-20 22:39:28 -0500

    Implement ToDoList.js

--- ToDoList/App.js
+++ ToDoList/App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import ToDoList from './components/ToDoList';
+import 'react-native-get-random-values';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ToDoList initialValues={["Eat", "Sleep"]}></ToDoList>
     </View>
   );
 }
